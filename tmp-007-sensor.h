6869bc4c3f17a5a9d1ee2c8329e1692ae27aac4c
babiskg <babiskagelidis@gmail.com>
2020-07-15 21:04:54 +0300

    Create tmp-007-sensor.h

---
+++ tmp-007-sensor.h
@@ -0,0 +1,10 @@
+#ifndef TMP_007_SENSOR_H_
+#define TMP_007_SENSOR_H_
+/*---------------------------------------------------------------------------*/
+#define TMP_007_SENSOR_TYPE_OBJECT    1
+#define TMP_007_SENSOR_TYPE_AMBIENT   2
+#define TMP_007_SENSOR_TYPE_ALL       3
+/*---------------------------------------------------------------------------*/
+extern const struct sensors_sensor tmp_007_sensor;
+/*---------------------------------------------------------------------------*/
+#endif /* TMP_007_SENSOR_H_ */
